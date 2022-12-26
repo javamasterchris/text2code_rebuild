3adb48c7f8d2f352000bede0189dbebc3a1eb2a8
Chris Kim <chrisck@umich.edu>
2022-12-25 22:22:59 -0500

    updated dependency list

--- extension.js
+++ extension.js
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const openAI = require('openai')
+const MarkdownIt = require('markdown-it')
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
