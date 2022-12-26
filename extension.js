10035f0a307cfd496ddf3f6fa0bd2174e29eef87
Chris Kim <chrisck@umich.edu>
2022-12-25 22:56:12 -0500

    mostly done. bug with the api key

--- extension.js
+++ extension.js
@@ -2,7 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const openAI = require('openai')
-const MarkdownIt = require('markdown-it')
+const MarkdownIt = require('markdown-it');
+const { validateHeaderName } = require('http');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -25,9 +26,70 @@ function activate(context) {
 		vscode.window.showInformationMessage('Hello World from text2code!');
 	});
 
+	let disposable2 = vscode.commands.registerCommand('text2code.text2code', () => {
+		vscode.window.showInputBox({
+			placeHolder: "What code do you want?"
+		}).then((input) => {
+			search(input);
+		})
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
+function search(input) {
+	const config = vscode.workspace.getConfiguration('text2code');
+	if(!validate(config)) return;
+	if(input) {
+		let disposableStatusMessage = vscode.window.setStatusBarMessage("Converting to code...")
+		const openai = generateOpenAI(config);
+		generateResponse(input, openai, disposableStatusMessage);
+	}
+}
+
+function validate(config) {
+	if(!config.apikey) {
+		vscode.window.showErrorMessage("No Open AI key provided in settings.")
+		return false;
+	}
+	return true;
+}
+
+function generateOpenAI(config) {
+	const configuration = new openAI.Configuration({
+		apiKey: config.apikey,
+	})
+	const openai = new openAI.OpenAIApi(configuration);
+	return openai;
+}
+
+function generateResponse(input, openAI, disposableStatusMessage) {
+	openAI.createCompletion({
+		model: "text-davinci-003",
+		prompt: input,
+		temperature: 0.7,
+		max_tokens:256,
+		top_p: 1,
+		frequency_penality:0,
+		presence_penalty:0,
+	}).then((response) => {
+		displayResult(response);
+		disposableStatusMessage.dispose();
+	}).catch((error) => {
+		vscode.window.showErrorMessage(error.response.data.error.message);
+		disposableStatusMessage.dispose();
+	})
+}
+
+function displayResult(openAIResponse) {
+	let panel = vscode.window.createWebviewPanel('webview', 
+												'AI Result', 
+												{ preserveFocus: true, viewColumn: vscode.ViewColumn.One});
+	const md = new MarkdownIt();
+	const result = md.render('```\n'  + (openAIResponse.data.choices[0].text || "Nothing found") + '\n```' );
+	panel.webview.html = result;
+}
 // This method is called when your extension is deactivated
 function deactivate() {}
 
